118060ac04cf4fd2bc79c731bd81d282a3862d14
dmko <kotyshok@yandex.ru>
2025-10-23 14:09:21 +0300

    Init commit

---
+++ src/main/java/ru/kot1a/market_data/MarketDataApp.java
@@ -0,0 +1,12 @@
+package ru.kot1a.market_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(MarketDataApp.class, args);
+    }
+}
